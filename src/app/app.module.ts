1e6bfb7ff5214a096ddfe4a65acd515a0fa91e1d
Lupe <luraquel@hotmail.com>
2024-07-17 22:35:10 -0500

    CAP. 66

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
+
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 
@@ -12,13 +14,13 @@ import { HeroesModule } from './heroes/heroes.module';
   declarations: [
     AppComponent,
 
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+import { ListComponent } from './components/list/list.component';
+import { MainPageComponent } from './pages/main-page-component';
+
+
+
+
+@NgModule({
+  declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
+  exports:[MainPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DbzModule { }
